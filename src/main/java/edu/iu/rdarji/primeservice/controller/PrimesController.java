d03c2dd0673c0cae9b1a1255a835453672d8fc8d
Rohan Darji <rohandarji2001@gmail.com>
2024-02-23 20:41:44 -0500

    changes

--- src/main/java/edu/iu/rdarji/primeservice/controller/PrimesController.java
+++ src/main/java/edu/iu/rdarji/primeservice/controller/PrimesController.java
@@ -1,7 +1,12 @@
 package edu.iu.rdarji.primeservice.controller;
 
 import edu.iu.rdarji.primeservice.service.IPrimesService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
