0c4d279cc3a7b97485b5a776b18b7e863cd05bb8
Rohan Darji <rohandarji2001@gmail.com>
2024-04-10 02:25:37 -0400

    tests

--- src/test/java/edu/iu/rdarji/primeservice/PrimeServiceApplicationTests.java
+++ src/test/java/edu/iu/rdarji/primeservice/PrimeServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package edu.iu.rdarji.primeservice;
 
+import edu.iu.rdarji.primeservice.service.PrimesService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PrimeServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	PrimesService primesService = new PrimesService();
+
+	@Test
+	void _45IsNotPrime() {
+		int n = 45;
+		boolean expected = false;
+		boolean actual = primesService.isPrime(n);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void _536225IsNotPrime() {
+		long n = 536225;
+		boolean expected = false;
+		boolean actual = primesService.isPrime(n);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void _285191IsPrime() {
+		long n = 285191;
+		boolean expected = true;
+		boolean actual = primesService.isPrime(n);
+		assertEquals(expected, actual);
 	}
 
 }
